7b7bf26453f9edebe93c802dcfae54df0570962f
Yuri Fernandes <yuri.fernandes@primecontrol.com.br>
2021-11-02 15:06:09 -0300

    CRUD My Todo

---
+++ MyTodo/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTodo.Models;
+
+namespace MyTodo.Data
+{
+    // Classe de representação do banco de dados em memória
+    public class AppDbContext : DbContext
+    {
+        // Propriedade que representa a tabela do banco de dados
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+        
+    }
+}
---
+++ MyTodo/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTodo.Models
+{
+    // Classe de representação das colunas do banco de dados
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
